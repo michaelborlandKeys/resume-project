a1d16fd8bd3490c7e3546b74306564a6a6204071
ICECUBE\micha_000 <michael.borland@my.simpson.edu>
2017-11-06 14:34:34 -0600

    commit 1

---
+++ public_html/js/Guessing_game.js
@@ -0,0 +1,148 @@
+var mikes_target = 0; 
+var number_of_guesses = 0; 
+var guess_count= 0; 
+
+
+
+
+
+function generate_Michael_Target()
+{
+  	var randomNumber = Math.floor(Math.random()*99)+1;
+  	mikes_target = randomNumber; 
+     
+
+	console.log(mikes_target); 
+} 
+
+var generate_random_number =  $('#playGuess'); 
+generate_random_number.on("click",generate_Michael_Target);
+
+function generate_New_Target()
+{
+
+  	var random_new_target = Math.floor(Math.random()*99)+1;
+  	mikes_target = random_new_target;
+  	 $('#player_input').val(""); 
+  	 $('#Michael_number').val("");
+     number_of_guesses = 0; 
+     guess_count= 0; 
+  	 document.getElementById("player_input").disabled = false; 
+  	 document.getElementById("clear_guess_Button").disabled = false; 
+  	 document.getElementById("guessButton").disabled = false; 
+     // $('#play_guessing_game').modal('show');
+
+
+//
+	console.log(mikes_target); 
+}
+var generate_new_number = $('#new_number'); 
+generate_new_number.on("click",generate_New_Target); 
+
+ function player_Guess()
+ {
+ 	
+ 	  if($('#player_input').val() > 99 || $('#player_input').val() <= 0 )
+ 	 {
+ 	 		$('#player_input').val("Sorry Values to guess 1-99"); 
+      
+ 	 		//console.log(mikes_target);
+        guess_count++; 
+     number_of_guesses=guess_count; 
+    console.log("Guess Total "+number_of_guesses);
+
+ 	 }
+ 	 
+ 	 else if($('#player_input').val() < mikes_target)
+ 	 {
+	 	 	$('#Michael_number').val("Go Higher"); 
+	 	 	//console.log(mikes_target); 
+     guess_count++; 
+     number_of_guesses=guess_count; 
+      console.log("Guess Total "+number_of_guesses);
+ 	 }
+
+ 	 else if($('#player_input').val() > mikes_target)
+ 	 {
+ 	 		$('#Michael_number').val("Go Lower"); 
+     guess_count++; 
+     number_of_guesses=guess_count; 
+ 	 		//console.log(mikes_target);
+      console.log("Guess Total "+number_of_guesses);
+
+ 	 }
+ 	  else if($('#player_input').val() == mikes_target)
+ 	 {
+ 	 		//$('#Michael_number').val("Congratulations you found Mike's Number"+" "+mikes_target); 
+ 	 		//console.log(mikes_target);
+       guess_count++; 
+       number_of_guesses=guess_count; 
+      console.log("Guess Total "+number_of_guesses);
+      window.alert("It took you"+" "+number_of_guesses+" "+"Guesses to Find Mikes Number: "+mikes_target+"Congratulations"); 
+        $('#play_guessing_game').modal('hide');
+        $('#player_input').val(""); 
+        $('#Michael_number').val("");
+         number_of_guesses = 0; 
+         guess_count= 0;
+         mikes_target = 0;  
+         document.getElementById("player_input").disabled = false; 
+         document.getElementById("clear_guess_Button").disabled = false; 
+         document.getElementById("guessButton").disabled = false; 
+
+      
+ 	 }
+
+ 	 else
+ 	 {
+ 	 	$('#player_input').val("Only enter numbers 1-99"); 
+ 	 	//console.log($('#player_input').val())
+     guess_count++; 
+     number_of_guesses=guess_count
+    console.log("Guess Total "+number_of_guesses);
+
+ 	 }
+ 	  
+ }
+var guess_button = $('#guessButton'); 
+guess_button.on("click",player_Guess); 
+
+function clearing_your_Guess()
+{
+
+ 	$('#player_input').val(""); 
+
+}
+var clear_guess = $('#clear_guess_Button'); 
+clear_guess.on("click",clearing_your_Guess); 
+
+function getAnwser()
+{
+	$('#Michael_number').val("Nice try, try again by clicking New Number"+" "+mikes_target); 
+	document.getElementById("player_input").disabled = true; 
+  	 document.getElementById("clear_guess_Button").disabled = true; 
+  	 document.getElementById("guessButton").disabled = true; 
+	$('#player_input').val("Field disabled, because you got the answer"); 
+  window.alert("You gave up after"+" "+number_of_guesses+" "+"Guesses"); 
+
+	
+}
+var answer_Button= $('#answerButton'); 
+answer_Button.on("click",getAnwser); 
+
+function guessReset()
+{
+	 $('#player_input').val(""); 
+  	$('#Michael_number').val("");
+     number_of_guesses = 0; 
+     guess_count= 0;
+     mikes_target = 0;  
+  	 document.getElementById("player_input").disabled = false; 
+  	 document.getElementById("clear_guess_Button").disabled = false; 
+  	 document.getElementById("guessButton").disabled = false; 
+
+}
+var guess_game_X= $('#X'); 
+guess_game_X.on("click", guessReset);
+var guess_game_close= $('#exitGuess'); 
+guess_game_close.on("click",guessReset);
+
---
+++ public_html/js/Guessing_game_themes.js
@@ -0,0 +1,114 @@
+var  player = window.prompt("Please enter a Username, otherwise a default one is entered for you.","Player 1"); 
+var theme = "";
+var lastvisited_theme = ""; 
+
+function gameTitle()
+{ 
+	theme="Default Theme"; 
+	$('#player_input').val(""); 
+	$('#Michael_number').val(""); 
+
+	if(player.length <= 10  && player.length >= 5)  {	
+		document.getElementById("versus_player").innerHTML= "Michael's Guessing Game's";
+		document.getElementById("test_player_heading").innerHTML="Current Player:"+" "+player;
+		 document.getElementById("test_theme_heading").innerHTML="Current Theme:"+" "+theme; 
+		window.alert("Success"); 
+	
+
+
+	}
+
+	else
+	{
+		
+		window.alert("Failed to enter a Username 5 to 10 long,Retry!");
+		
+	}
+}
+
+var versus_player = $('#playGuess');
+versus_player.on("click",gameTitle); 
+
+function halloweenTheme()
+{
+	//Halloween Theme
+	 theme="Halloween Theme"; 
+
+ 
+	$("#versus_player").css({"color": "black"});
+	$(".modal-header").css({"background-color": "orange","border-bottom":"black 1px solid"});
+	$(".modal-body").css({"background-color": "orange","color":"black"}); 
+	$('.modal-footer').css({"border-color":"black","background-color":"orange"}); 
+	$('.modal-content').css({"background-color": "orange"}); 
+	$('.btn').css({"background-color":"black","color":"white","opacity":"1"}); 
+	$(document).ready(function(){
+    	$(".btn").hover(function(){
+        	$(this).css({"color": "red","opacity":".8"});
+        	}, function(){
+        	$(this).css({"color": "white","opacity":"1"});
+    		});
+				});
+	$(document).ready(function(){
+    	$(".close").hover(function(){
+        	$(this).css({"color": "red","opacity":".8"});
+        	}, function(){
+        	$(this).css({"color": "black","opacity":"1"});
+    		});
+				});
+
+	// end of css script 
+
+	if(theme=="Halloween Theme")
+	{	
+		window.alert("Enjoy this Brand new "+theme+"!"+" "+"Happy Halloween"+" "+player); 
+		document.getElementById("versus_player").innerHTML= "Michael's Guessing Game's"; 
+	 document.getElementById("test_player_heading").innerHTML="Current Player:"+" "+player;
+		document.getElementById("test_theme_heading").innerHTML="Current Theme:"+" "+theme; 
+		
+	}
+}
+
+function defaultTheme()
+
+{
+
+theme="Default Theme"; 
+lastvisited_theme="Halloween Theme"; 
+ 
+	$("#versus_player").css({"color": "#fdeb00"});
+	$(".modal-header").css({"background-color": "black","border-bottom":"white 1px solid"});
+	$(".modal-body").css({"background-color": "black","color":"#C62D42"}); 
+	$('.modal-footer').css({"border-color":"white","background-color":"black"}); 
+	$('.modal-content').css({"background-color": "black"}); 
+	$('.btn').css({"background-color":"#C62D42","color":"#fdeb00","opacity":".8"}); 
+	$(document).ready(function(){
+    	$(".btn").hover(function(){
+        	$(this).css({"color": "#fdeb00","opacity":".5"});
+        	}, function(){
+        	$(this).css({"color": "#fdeb00","opacity":".8"});
+    		});
+				});
+	$(document).ready(function(){
+    	$(".close").hover(function(){
+        	$(this).css({"color": "#C62D42","opacity":".7"});
+        	}, function(){
+        	$(this).css({"color": "#fdeb00","opacity":".7"});
+    		});
+				});
+
+	// end of css script 
+
+	if(theme=="Default Theme")
+	{	
+		window.alert("Hope you Enjoyed the "+lastvisited_theme+" "+"Welcome back to"+" "+theme+" "+player); 
+		document.getElementById("versus_player").innerHTML= "Michael's Guessing Game's";
+			document.getElementById("test_player_heading").innerHTML="Current Player:"+" "+player;
+		 document.getElementById("test_theme_heading").innerHTML="Current Theme:"+" "+theme; 
+
+	
+
+	}
+
+}
+
+
