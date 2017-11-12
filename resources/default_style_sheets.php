774dd26547a2de33959dcf886b2fd545cc493f72
ICECUBE\micha_000 <michael.borland@my.simpson.edu>
2017-11-11 23:59:16 -0600

    hjgjhghjg

--- resources/default_style_sheets.php
+++ resources/default_style_sheets.php
@@ -1,4 +1,4 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="css/default.css" />
-  <link rel="stylesheet" type="text/css" href="resources/public_html/index_resume.css" />
-   <link rel="stylesheet" type="text/css" href="resources/public_html/nav_bar_resume.css" />
+  <link rel="stylesheet" type="text/css" href="../resources/index_resume.css" />
+ 
