8bab9f8759802879650bd58d87b21f97b65db69a
ICECUBE\micha_000 <michael.borland@my.simpson.edu>
2017-11-12 14:38:56 -0600

    testing webhook

--- public_html/index.php
+++ public_html/index.php
@@ -14,7 +14,7 @@
    
 <div class="container">
      <div class="row"> 
-         <h1 id="page-header" style="text-decoration: underline;"> Michael's Resume Index</h1>
+         <h1 id="page-header" style="text-decoration: underline;"> Michael's Resume Index test</h1>
     </div>
         <div class="row" >
     <div class="col-sm-3 col-md-6 col-lg-12 dropdown" id="nav_button">
