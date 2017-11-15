51f475847326a8228f34ef23c5dc8d431bce3855
ICECUBE\micha_000 <michael.borland@my.simpson.edu>
2017-11-15 17:50:30 -0600

    fixing errors

--- resources/nav_bar_resume.php
+++ resources/nav_bar_resume.php
@@ -6,7 +6,7 @@
                 <ul>
                     <li class="dropdown">
                         <a href="#">Developer's Resume</a>
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="caret"></a>
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></a>
                     <ul class="dropdown-menu">
                     <li class="dropdown-header" id="main_dropdown_header">Developer's Resume Information</li>
                     <li class="dropdown-item"><a href="index.php">Resume Index</a></li>
@@ -20,7 +20,7 @@
                 <li class="dropdown">
                     <a href="#">Programs Originally Written in C#</a>
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                    <span class="caret"></a> 
+                    <span class="caret"></span></a> 
                     <ul class="dropdown-menu">  
                     <li class="dropdown-header" id="C_dropdown_header">Programs Originally Written in C#</li>
                     <li class="dropdown-item"><a href="guessing_game.php"> Michael's Guessing Game</a></li>
@@ -31,7 +31,7 @@
                <li class="dropdown">
                     <a href="#">Programs Originally in Python</a>
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                    <span class="caret"></a>
+                    <span class="caret"></span></a>
                       <ul class="dropdown-menu"> 
                       <li class="dropdown-header" id="python_nav_header">Programs Originally in Python</li> 
                  
@@ -40,7 +40,7 @@
               <li class="dropdown">
                     <a href="#">Other Programs written to display other Skills</a>
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                    <span class="caret"></a>
+                    <span class="caret"></span></a>
                       <ul class="dropdown-menu"> 
                       <li class="dropdown-header" id="other_nav_header">Other Programs written to display other Skills</li> 
                         <li class="dropdown-header" id="college_nav_header">Other Sites Developed in College</li>
@@ -50,7 +50,7 @@
                <li class="dropdown">
                     <a href="#">Programs Originally in Java</a>
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                    <span class="caret"></a>
+                    <span class="caret"></span></a>
                       <ul class="dropdown-menu"> 
                       <li class="dropdown-header" id="java_nav_header">Programs Originally in Java</li> 
                  
