a7f685123ad0205424076f62470847c69f7adef4
ICECUBE\micha_000 <michael.borland@my.simpson.edu>
2017-11-15 17:42:59 -0600

    need to find image i can use moving directory

--- public_html/home.php
+++ public_html/home.php
@@ -20,6 +20,17 @@
     </div>
       <div class="site_content_summary" id="summary_list">
     <h2 class="site_checklist" id="style_summary">Michael's Resume Site Summary</h2>
+    <p>This site will be continuously updated to expand my knowledge and available for reference, for what i have done, and what i am learning if it pertains to apache php framework.I will link to other sites, i have developed from this one.  I have learned on Apache and php with Java and Tomcat thrown in, but mainly Apache, and PHP. I am willing to work on any web stack, i am just looking for work in the field and will adapt to any stack employers need development to take place in.I am learning  .net framework and getting much more familiar with all stuff i got a good introduction to in college JavaScript,CSS,HTML5,PHP,and mySQL.   </p>
+        <ul>
+            <li>Show in detail my ability to program with proficiently in many different web based languages.</li>
+            <li>Specifically using JavaScript,CSS,HTML5,PHP,and mySQL </li>
+            <li>This site will link out to my site i am learning .net framework on, given this is apache i can't run .net, i need IIS server. </li>
+            <li>Libraries heavily featured in this website are Jquery, Bootstrap, and more to be filled in. </li>
+            <li> I will explain my experience in college development, and have many application examples converted from what was originally programmed in to JavaScript.</li>
+            <li>I will then use different Libraries available in JavaScript to learn more, and expand my applications capabilities and knowledge </li>
+            <li>Build new improved version of old programs to give overview of my main focus in college development wise. develop many new applications to show progress and advancement of programming knowledge.</li>
+            <li>Finally, touch on other development in more detail i have done</li>
+        </ul>
 
 
 
@@ -32,9 +43,9 @@
     <?php include("../resources/default_script_loading.php") ?>
         <!--JavaScript I programmed --><!-- 
        <script src="js/hovering_style_change.js" type="text/javascript" charset="utf-8" async defer></script>  -->
-       <script>
-           $( "#summary_list" ).load( "../resources/documents_and_notes/test.html" );
-       </script>
+    <!--    <script>
+           // $( "#summary_list" ).load( "../resources/documents_and_notes/test.html" );
+       </script> -->
 
   
        
